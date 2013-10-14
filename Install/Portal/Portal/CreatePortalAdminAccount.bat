7d83ddfdb3e6e3829a24538cae1cd674cb92d21b
Eric Linz <elinz@esri.com>
2013-10-14 08:03:19 -0700

    Fix to issue #83

--- Install/Portal/Portal/CreatePortalAdminAccount.bat
+++ Install/Portal/Portal/CreatePortalAdminAccount.bat
@@ -9,7 +9,19 @@ echo Create the Portal for ArcGIS primary administrator account...
 echo.
 echo ^- When the web browser opens the "Initial Administrator Account Required" page...
 echo.
+echo    NOTE: The web browser will open in a minute or two; a delay has been
+echo          added to give the portal service time to restart.
+echo.
 echo    1. Enter the required information to create the admin user.
+echo.
+echo       ***************          WARNING         ***************
+echo       *** User/password for portal administrator account must be the
+echo       *** same as that used for the ArcGIS Server site.
+echo       ***
+echo       *** User must be set as: %ops_userName%
+echo       *** Password must be set as: %ops_passWord%
+echo       ***
+echo       ********************************************************            
 echo.
 echo    2. Click "Create".
 echo.
@@ -22,9 +34,9 @@ echo    5. Sign Out of the portal.
 echo.
 echo    6. Close the web browser.
 echo.
+PING 127.0.0.1 -n 80 > nul
 set execute=%ops_webBrowserExePath% https://%ops_FQDN%:7443/arcgis/home/signin.html?
 echo %execute%
 echo.
 %execute%
 Call %ops_ChkErrLevelFile% %ERRORLEVEL%
-PING 127.0.0.1 -n 3 > nul
