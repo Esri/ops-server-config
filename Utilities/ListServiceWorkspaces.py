24ad9620b946a0d56a340916e4566cc103cc507f
Eric Linz <elinz@esri.com>
2014-08-22 13:48:44 -0700

    Resolved #298
    Create script to list the ArcGIS Server service workspaces (Utility
    script).

---
+++ Utilities/ListServiceWorkspaces.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+#==============================================================================
+#Name:          ListServiceWorkspaces.py
+#Purpose:       
+#
+#Prerequisites: 
+#               
+#
+#History:       2017/07/06:   Initial code.
+#
+#==============================================================================
+import sys, os, traceback, datetime, ast, copy, json
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), 'SupportFiles'))
+
+from AGSRestFunctions import getServiceList
+from AGSRestFunctions import getServiceManifest
+
+scriptName = os.path.basename(sys.argv[0])
+exitErrCode = 1
+debug = False
+sectionBreak = '=' * 175
+sectionBreak1 = '-' * 175
+
+
+def check_args():
+    # ---------------------------------------------------------------------
+    # Check arguments
+    # ---------------------------------------------------------------------
+
+    if len(sys.argv) <> 6:
+        
+        print '\n' + scriptName + ' <Server_FullyQualifiedDomainName> <Server_Port> <User_Name> <Password> <Use_SSL: Yes|No>'
+    
+        print '\nWhere:'
+        print '\n\t<Server_FullyQualifiedDomainName> (required): the fully qualified domain name of the ArcGIS Server machine.'
+        print '\n\t<Server_Port> (required): the port number of the ArcGIS Server (specify # if no port).'
+        print '\n\t<User_Name> (required): ArcGIS Server for ArcGIS site administrator.'
+        print '\n\t<Password> (required): Password for ArcGIS Server for ArcGIS site administrator user.'
+        print '\n\t<Use_SSL: Yes|No> (required) Flag indicating if ArcGIS Server requires HTTPS.\n'
+        return None
+    
+    else:
+        
+        # Set variables from parameter values
+        server = sys.argv[1]
+        port = sys.argv[2]
+        adminuser = sys.argv[3]
+        password = sys.argv[4]
+        useSSL = sys.argv[5]
+        
+        if port.strip() == '#':
+            port = None
+        
+        if useSSL.strip().lower() in ['yes', 'ye', 'y']:
+            useSSL = True
+        else:
+            useSSL = False
+        
+    return server, port, adminuser, password, useSSL
+
+def parseService(service):
+    # Parse folder and service nameType
+    folder = None
+    serviceNameType = None
+     
+    parsedService = service.split('//')
+    
+    if len(parsedService) == 1:
+        serviceNameType = parsedService[0]
+    else:
+        folder = parsedService[0]
+        serviceNameType = parsedService[1]
+        
+    return folder, serviceNameType
+
+def main():
+    
+    totalSuccess = True
+    
+    # -------------------------------------------------
+    # Check arguments
+    # -------------------------------------------------
+    results = check_args()
+    if not results:
+        sys.exit(exitErrCode)
+    server, port, adminuser, password, useSSL = results
+    
+    if debug:
+        print server, port, adminuser, password, useSSL
+    
+    #print
+    #print '=' * 100
+    #print ' List Service Workspaces'
+    #print '=' * 100
+    #print
+    
+    try:            
+        # ------------------------------------------------- 
+        # Get all services that exist on server
+        # -------------------------------------------------
+        if useSSL:
+            protocol = 'https'
+        else:
+            protocol = 'http'
+            
+        allServices = getServiceList(server, port, adminuser, password)
+        
+        # Remove certain services from collection
+        excludeServices = ['SampleWorldCities.MapServer']
+        services = [service for service in allServices if service not in excludeServices]
+        if len(services) == 0:
+            raise Exception('ERROR: There are no user published ArcGIS Server services. Have you published the ArcGIS Server services?')
+        
+        # -------------------------------------------------
+        # List service workspaces
+        # -------------------------------------------------
+        numServices = len(services)
+        #print '\n- List service workspaces...\n'
+        i = 0
+        
+        # Print header
+        print 'ArcGIS Server|Service|On Server Connection String/or Path'
+        
+        for service in services:
+            onServerStr = ''
+            #i = i + 1
+            #print '\n{}'.format('-' * 116)
+            #print 'Service: {:<100}{:<10}'.format(service, '{}/{}'.format(i, numServices))
+            
+            folder, serviceNameType = parseService(service)
+            
+            serviceManifest = getServiceManifest(server, port, adminuser, password, folder, serviceNameType)
+
+            databases = serviceManifest.get('databases')
+            if databases:
+                onServerConnStr = databases[0].get('onServerConnectionString')
+                if onServerConnStr:
+                    if onServerConnStr.find('DB_CONNECTION_PROPERTIES') > 0:
+                        # It's an enterprise geodatabase
+                        onServerStr = onServerConnStr.split('DB_CONNECTION_PROPERTIES=')[1]
+                    else:
+                        onServerStr = onServerConnStr.replace('DATABASE=','')
+            else:
+                if serviceNameType.find('.GeocodeServer') > 0 or serviceNameType.find('.GPServer') > 0:
+                    onServerStr = serviceManifest.get('resources')[0].get('serverPath')
+            
+            if len(onServerStr) == 0:
+                onServerStr = str(serviceManifest)
+            
+            print '{}|{}|{}'.format(server, service, onServerStr)
+
+                    
+    except:
+        totalSuccess = False
+        
+        # Get the traceback object
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+     
+        # Concatenate information together concerning the error into a message string
+        pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info()[1])
+     
+        # Print Python error messages for use in Python / Python Window
+        print
+        print "***** ERROR ENCOUNTERED *****"
+        print pymsg + "\n"
+        
+    finally:
+        if totalSuccess:
+            #print "{} was completed successfully.".format(scriptName)
+            sys.exit(0)
+        else:
+            #print "ERROR: {} was _NOT_ completed successfully.".format(scriptName)
+            sys.exit(1)
+        
+        
+if __name__ == "__main__":
+    main()
