422559e24b940c2ae96742e0c62337fbc7284aae
Eric Linz <elinz@esri.com>
2014-12-15 12:46:43 -0800

    Resolved #646
    Add "abbreviated" Apache V2 License header to all
    ops-server-config\Utilities source files

--- Utilities/CreateBatchFileToRunCreateServiceList.py
+++ Utilities/CreateBatchFileToRunCreateServiceList.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
+#------------------------------------------------------------------------------
+# Copyright 2014 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #==============================================================================
-# Creates a batch file containing the syntax to execute the CreateServiceList.py
-# for each portal user in the specified portal.
+#Name:          CreateBatchFileToRunCreateServiceList.py
+#
+#Purpose:       Creates a batch file containing the syntax to execute the CreateServiceList.py
+#               for each portal user in the specified portal.
+#
 #==============================================================================
 import sys
 import os
